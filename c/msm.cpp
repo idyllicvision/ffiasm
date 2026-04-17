a7f616817c2b207f6edb3806d02361afcd139dc3
furze <danielsennmurilio@gmail.com>
2026-04-17 06:29:36 +0300

    use exact parallel MSM cost check for cache path

--- c/msm.cpp
+++ c/msm.cpp
@@ -3,16 +3,16 @@
 #include "misc.hpp"
 
 template <typename Curve, typename BaseField>
-typename MSM<Curve, BaseField>::CostEstimate
-MSM<Curve, BaseField>::estimateCost(uint8_t* _scalars,
-                                    uint64_t _scalarSize,
-                                    uint64_t _n,
-                                    uint64_t _bitsPerChunk)
+uint64_t MSM<Curve, BaseField>::estimateCost(uint8_t* _scalars,
+                                             uint64_t _scalarSize,
+                                             uint64_t _n,
+                                             uint64_t _bitsPerChunk)
 {
-    CostEstimate est;
-
     const uint64_t nPoints = _n;
-    if (nPoints == 0) return est;
+    if (nPoints == 0) return 0;
+
+    ThreadPool &threadPool = ThreadPool::defaultPool();
+    const uint64_t nThreads = threadPool.getThreadCount();
 
     scalars = _scalars;
     scalarSize = _scalarSize;
@@ -20,51 +20,54 @@ MSM<Curve, BaseField>::estimateCost(uint8_t* _scalars,
 #ifdef MSM_BITS_PER_CHUNK
     bitsPerChunk = MSM_BITS_PER_CHUNK;
 #else
-    bitsPerChunk = calcBitsPerChunk(nPoints, scalarSize);
+    bitsPerChunk = _bitsPerChunk ? _bitsPerChunk : calcBitsPerChunk(nPoints, scalarSize);
 #endif
 
     if (nPoints == 1) {
-        // This is a special case of mulByScalar; the regular MSM model may not very applicable here
-        // We keep the estimate minimal
-        return est;
+        return 1;
     }
 
     const uint64_t nChunks = calcChunkCount(scalarSize, bitsPerChunk);
     const uint64_t nBuckets = calcBucketCount(bitsPerChunk);
 
-    for (uint64_t i = 0; i < nPoints; i++) {
-        int carry = 0;
+    std::unique_ptr<uint64_t[]> partial(new uint64_t[nThreads]);
+    for (uint64_t t = 0; t < nThreads; t++) partial[t] = 0;
 
-        for (uint64_t j = 0; j < nChunks; j++) {
-            int bucketIndex = (int)getBucketIndex(i, j) + carry;
+    threadPool.parallelFor(0, nPoints, [&] (int begin, int end, int numThread) {
+        uint64_t localNonZero = 0;
 
-            if (bucketIndex >= (int)nBuckets) {
-                bucketIndex -= (int)(nBuckets * 2);
-                carry = 1;
-            } else {
-                carry = 0;
-            }
+        for (int i = begin; i < end; i++) {
+            int carry = 0;
 
-            if (bucketIndex != 0) {
-                est.nonZeroSlices++;
+            for (uint64_t j = 0; j < nChunks; j++) {
+                int bucketIndex = (int)getBucketIndex(i, j) + carry;
+
+                if (bucketIndex >= (int)nBuckets) {
+                    bucketIndex -= (int)(nBuckets * 2);
+                    carry = 1;
+                } else {
+                    carry = 0;
+                }
+
+                if (bucketIndex != 0) {
+                    localNonZero++;
+                }
             }
         }
+
+        partial[numThread] += localNonZero;
+    });
+
+    uint64_t nonZeroSlices = 0;
+    for (uint64_t t = 0; t < nThreads; t++) {
+        nonZeroSlices += partial[t];
     }
 
-    // in every chunk:
-    // g.copy(t, buckets[last]);
-    // then for i = last-1..0:
-    //   g.add(tmp, tmp, buckets[i]);
-    //   g.add(t, t, tmp);
-    // => 2 * (nBuckets - 1) adds per chunk
-    est.bucketReduceAdds = nChunks * 2 * (nBuckets - 1);
+    const uint64_t bucketReduceAdds = nChunks * 2 * (nBuckets - 1);
+    const uint64_t chunkMergeAdds = (nChunks > 0) ? (nChunks - 1) : 0;
+    const uint64_t chunkMergeDbls = (nChunks > 0) ? ((nChunks - 1) * bitsPerChunk) : 0;
 
-    if (nChunks > 0) {
-        est.chunkMergeAdds = nChunks - 1;
-        est.chunkMergeDbls = (nChunks - 1) * bitsPerChunk;
-    }
-
-    return est;
+    return nonZeroSlices + bucketReduceAdds + chunkMergeAdds + chunkMergeDbls;
 }
 
 template <typename Curve, typename BaseField>
